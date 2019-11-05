591fa4d3744e9b14bf4a9b92d2a7dfc30bcbeff6
masonpam <masonp@seattleu.edu>
2019-11-04 16:57:36 -0800

    lab6

--- lab6_1.py
+++ lab6_1.py
@@ -6,24 +6,22 @@ def divisible_by_7(num):
       return None 
 
 def compare_it(x, y):
-   if type(x) is not int and type(y) is not int:
+   if not isinstance(x, int) and not isinstance(y, int):
       return False
    if x != y:
       return False 
-   if x <= 0 and y <= 0:
+   if x < 0 and y < 0:
       return False
    else:
       return True
 
-# Had a lot of trouble and couldn't figure this one out
-def keyword_counter(a, b, c):
-   fhand = open('test6_1.txt')
-   counts = dict()
-   names = [keyword]
-   for names in fhand:
-      counts[name] = counts.get(name, 0) + 1 
-   print(counts)   
-with open(file_name, "r") as f:
-   text = f.read().replace("\n", " ")
-
+def keyword_counter(word_list, logic, test): 
+   data = test
+   if not logic:
+      with open(test, "r") as f:
+         text = f.read().replace("\n", " ")
+results = {}
+for word in word_list:
+   results[word] = text.count(word)
+return results
 
